adb1b2a3339fadd21b2930719ea5f98850f432cd
M.Sc. Damir Olejar <olejar.damir@gmail.com>
2024-03-09 09:30:00 -0500

    Update install1.sh

--- in_progress/install1.sh
+++ in_progress/install1.sh
@@ -1,35 +1,28 @@
 #!/bin/bash
 
-# Step 1: Install Golang
-echo "Step 1: Install Golang"
-if command -v go &> /dev/null; then
-    echo "Golang is already installed. Skipping installation."
-else
-    GO_VERSION=$(curl -s -S -L https://github.com/pingcap/tidb/blob/master/go.mod | grep -Eo "\"go [[:digit:]]+.[[:digit:]]+\"" | grep -Eo "[[:digit:]]+.[[:digit:]]+")
-    echo "Downloading and installing Go ${GO_VERSION}..."
-    curl -s -S -L https://golang.org/dl/go${GO_VERSION}.linux-amd64.tar.gz | sudo tar -C /usr/local -xz
-    export PATH=$PATH:/usr/local/go/bin
-fi
+# Example for Ubuntu/Debian:
+sudo apt update
+sudo apt install -y git curl
 
-# Step 2: Manage the Go toolchain using gvm
-echo "Step 2: Manage the Go toolchain using gvm"
-if command -v gvm &> /dev/null; then
-    echo "gvm is already installed. Skipping installation."
-else
-    echo "Installing gvm..."
-    curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer | bash
-    source ~/.gvm/scripts/gvm
-fi
+# Remove any existing Go installation
+sudo rm -rf /usr/local/go
 
-TIDB_GOVERSION=$(curl -s -S -L https://github.com/pingcap/tidb/blob/master/go.mod | grep -Eo "\"go [[:digit:]]+.[[:digit:]]+\"" | grep -Eo "[[:digit:]]+.[[:digit:]]+")
+# Specify the Go version
+TIDB_GOVERSION="1.22.1"
 
-echo "Installing and setting Go ${TIDB_GOVERSION} as default..."
-gvm install go${TIDB_GOVERSION}
-gvm use go${TIDB_GOVERSION} --default
+# Download and extract the specified Go version
+GO_URL="https://go.dev/dl/go${TIDB_GOVERSION}.linux-amd64.tar.gz"
 
-# Step 3: Verification
-echo "Step 3: Verification"
-echo "Verifying Go installation..."
+# Download and extract the Go archive
+sudo wget $GO_URL -O go.tar.gz
+sudo tar -C /usr/local -xzf go.tar.gz
+sudo rm go.tar.gz
+
+# Add /usr/local/go/bin to the PATH environment variable
+export PATH=$PATH:/usr/local/go/bin
+
+# Add the export PATH command to ~/.bashrc for persistence
+echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
+
+# Verify the installation
 go version
-
-echo "Installation completed successfully!"
